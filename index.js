57dd17e47176e52ee4cbdb657e74f528147d78d6
Sachin Gurung <saching061@hotmail.com>
2022-04-27 13:53:51 -0700

    Added a dev script and set up a dynamic PORT for deploy

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
+// console.log(process.env.PORT);
+
 // create an express server from the express function above
 const server = express(); // this server is deaf AF. Can't hear ANYTHING. It's locked out of the world.
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
